71d19b532680314fb5377d07ae994aad04d784c1
Woodson Juste <j.woodson@hotmail.fr>
2022-12-13 10:41:57 +0100

    Menu + Fichier

---
+++ FormationWPF/Fichier/01_OpenFileDialog.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FormationWPF.Fichier
+{
+    /// <summary>
+    /// Logique d'interaction pour _01_OpenFileDialog.xaml
+    /// </summary>
+    public partial class _01_OpenFileDialog : Window
+    {
+        public _01_OpenFileDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            //Definir le repertoire initial 
+            openFileDialog.InitialDirectory = @"C:\Users\Admin stagiaire.DESKTOP-8967908\Desktop\MiseEnPratique\FormationWPF_12_12_2022\Docs";
+
+            //Definir un filtre pour l'extension du fichier 
+            openFileDialog.Filter = "Text file(*txt) | *.txt| All files(*.*) | *.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                //Contient le  chemin du fichier 
+                FileNameTextBox.Text = openFileDialog.FileName;
+
+                TextBlock.Text = File.ReadAllText(openFileDialog.FileName, Encoding.GetEncoding("iso-8859-1"));
+            } else
+            {
+                MessageBox.Show("Import fichier txt Annulé");
+            }
+        }
+    }
+}
--- FormationWPF/MainWindow.xaml.cs
+++ FormationWPF/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FormationWPF.Controles;
+using FormationWPF.Fichier;
+using FormationWPF.MiseEnPage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +26,109 @@ namespace FormationWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /*
+         * Les boites de dialiogue sont des fenêtres d'affichages
+         * Elles peuvent s'afficher en mode  "Modal" ou "Non Modal"
+         * Il existe deux méthodes permettant d'afficher une fenêtre soient en mode modal ou non modal: 
+         *          - La méthode Show : permet d'afficher une fenêtre en mode "non modal "
+         *                                             Cela signifie que vous pouvez basculer ou activer les autres fenêtre  de l'application 
+         *                                              même lorsque la fenêtre actuelle est une fenêtre active
+         *          - La méthode ShowDialog : Ouvre une fenêtre en tant que fenêtre "modale"
+         *                                                      Cela signifie que lorsque la fenêtre est active, tous les autres fenêtre de l'application sont 
+         *                                                      desactivées. On ne peut pas basculer vers d'autres fenêtres tant que la fenêtre actuelle 
+         *                                                      n'est pas fermée.
+         */
+
+        private void miModale_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationWindow w = new NavigationWindow();
+            w.ShowDialog();
+        }
+
+        private void miNonModale_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationWindow w = new NavigationWindow();
+            w.Show();
+        }
+
+        private void miBoiteSimpte_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("WPF, c'est facile!");
+        }
+
+        private void miBoiteP_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult rs =  MessageBox.Show("Etes vous prêt à sauter ? ", "Titre", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if(rs == MessageBoxResult.Yes)
+            {
+                MessageBox.Show("Vous n'auriez pas dû!!!");
+            } else
+            {
+                MessageBox.Show("Excellente décision!!!");
+            }
+        }
+
+        private void miQuitter_Click(object sender, RoutedEventArgs e)
+        {
+            // this.Close();
+            //  ou
+            Application.Current.Shutdown();
+        }
+
+        private void miAncrage_Click(object sender, RoutedEventArgs e)
+        {
+            _01_Ancrage ancrage = new _01_Ancrage();
+            ancrage.ShowDialog();
+        }
+
+        private void miStackP_Click(object sender, RoutedEventArgs e)
+        {
+            _02_StackPanel stackPanel = new _02_StackPanel();   
+            stackPanel.ShowDialog();
+        }
+
+        private void miWrapPanel_Click(object sender, RoutedEventArgs e)
+        {
+            _03_WrapPanel wp = new _03_WrapPanel();  
+            wp.ShowDialog();
+        }
+
+        private void miDockPanel_Click(object sender, RoutedEventArgs e)
+        {
+            _04_DockPanel dockPanel = new _04_DockPanel();
+            dockPanel.ShowDialog();   
+        }
+
+        private void miGrid_Click(object sender, RoutedEventArgs e)
+        {
+            _05_Grid grid = new _05_Grid();
+            grid.ShowDialog();
+        }
+
+        private void miCanvas_Click(object sender, RoutedEventArgs e)
+        {
+            _06_Canvas canvas = new _06_Canvas();
+            canvas.ShowDialog();
+        }
+
+        private void miControleCommun_Click(object sender, RoutedEventArgs e)
+        {
+            _01_ControlesCommunsWPF  cm = new _01_ControlesCommunsWPF();
+            cm.ShowDialog();    
+        }
+
+        private void miOuvrir_Click(object sender, RoutedEventArgs e)
+        {
+            _01_OpenFileDialog openFileDialog = new _01_OpenFileDialog();
+            openFileDialog.ShowDialog();
+        }
+
+        private void miEnregistrer_Click(object sender, RoutedEventArgs e)
+        {
+            _02_SaveFileDialog saveFileDialog = new _02_SaveFileDialog();
+            saveFileDialog.ShowDialog();
         }
     }
 }
